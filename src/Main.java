764cd179e9972bc61268490302399d56798bb841
efeldel <man-9k@ya.ru>
2022-06-03 23:45:29 +0300

    arrows init

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,54 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame("Текстура");
+        int x = 0;
+        int y = 0;
+        JFrame frame = new JFrame("Управление стрелками");
         frame.setLayout(null);
         frame.setBounds(500,200,800,800);
+        frame.setBackground(Color.white);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
-        BufferedImage image = ImageIO.read(new File("grass.jpg"));
-        for (int i = 0; i < frame.getWidth()/50; i++) {
-            for (int j = 0; j < frame.getHeight()/50; j++) {
-                JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(50, 50, Image.SCALE_FAST)));
-                frame.add(label);
-                label.setBounds(i*50,j*50,50,50);
-            }
-        }
-        frame.setVisible(true);
+
+        JPanel panel = new JPanel(null);
+        panel.setBounds(0,0,frame.getWidth(),frame.getHeight());
+        panel.setFocusable(true);
+
+        BufferedImage image = ImageIO.read(new File("smile.jpg"));
+        JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(50, 50, Image.SCALE_FAST)));
+        label.setBounds(x,y,50,50);
+        panel.add(label);
+     //  while (true) {
+            panel.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    switch (e.getKeyCode()) {
+                        case KeyEvent.VK_LEFT:  // left
+                            label.setBounds(x - 50, y, 50, 50);
+                            break;
+                        case KeyEvent.VK_UP:  // up
+                            label.setBounds(x, y - 50, 50, 50);
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            label.setBounds(x + 50, y, 50, 50);
+                            break;
+                        case KeyEvent.VK_DOWN:  // down
+                            label.setBounds(x, y + 50, 50, 50);
+                            break;
+                    }
+                }
+
+            });
+            frame.getContentPane().add(panel);
+            frame.setVisible(true);
+   //     }
     }
 }

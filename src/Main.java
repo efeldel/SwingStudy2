1c00f0c7e4556a8273021c06cc582b7add550b3d
efeldel <man-9k@ya.ru>
2022-06-03 22:42:58 +0300

    texture

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Текстура");
+        frame.setLayout(null);
         frame.setBounds(500,200,800,800);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        try {
-            BufferedImage image = ImageIO.read(new File("grass.jpg"));
-        }
-        catch (IOException e) {
-            System.out.println("File not found");
-        }
 
+        BufferedImage image = ImageIO.read(new File("grass.jpg"));
+        for (int i = 0; i < frame.getWidth()/50; i++) {
+            for (int j = 0; j < frame.getHeight()/50; j++) {
+                JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(50, 50, Image.SCALE_FAST)));
+                frame.add(label);
+                label.setBounds(i*50,j*50,50,50);
+            }
+        }
+        frame.setVisible(true);
     }
 }
